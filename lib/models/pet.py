bb7e58e5c2f4cb1d87ea0157358d0de96e17ad95
Charlie Gray <charliechasegray@gmail.com>
2024-01-31 19:38:09 -0700

    decrease indices in pet class static method

--- lib/models/pet.py
+++ lib/models/pet.py
@@ -201,7 +201,7 @@ class Pet:
             result = CONN.execute(query)
             pets = []
             for row in result:
-                pet = Pet(row[0], row[1], row[2], row[3], row[4], row[5])
-                owner = Owner(str(row[7]), str(row[6]))
+                pet = Pet(row[0], row[1], row[2], row[3], row[4])
+                owner = Owner(str(row[6]), str(row[5]))
                 pets.append((pet, owner))
             return pets
